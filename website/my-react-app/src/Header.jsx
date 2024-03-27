dbc7af9c709898ce65abcf472198db9bbd66d147
William Crupi <crupib@gmail.com>
2024-03-27 16:16:09 -0400

    Header update

--- website/my-react-app/src/Header.jsx
+++ website/my-react-app/src/Header.jsx
@@ -3,8 +3,15 @@ function Header()
   return (
   <header>
    <h1>My Website</h1>
-   
-  </header>  
+   <nav>
+    <ul>
+      <li><a href="#">Home</a></li>
+      <li><a href="#">About</a></li>
+      <li><a href="#">Services</a></li>
+      <li><a href="#">Contact</a></li>
+    </ul>
+   </nav> 
+ </header>  
   );
 
 }
