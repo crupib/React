23decb90efe35a28c661d223c76927c48aa0b967
William Crupi <crupib@gmail.com>
2024-03-28 08:11:41 -0400

    my-card-app init

---
+++ my-app-card/src/Card.jsx
@@ -0,0 +1,10 @@
+function Card() {
+    return (
+        <div className="card">
+            <img></img>
+            <h2>Bill</h2>
+            <p>I am a homo and like transgender whores</p>
+        </div>
+    );
+}
+export function Card
