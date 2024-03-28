c88e29a7e62b1c363cccb241aab561e87f38183b
William Crupi <crupib@gmail.com>
2024-03-28 16:10:31 -0400

    horizontal

--- my-app-card/src/Card.jsx
+++ my-app-card/src/Card.jsx
@@ -2,7 +2,7 @@ import profilePic from './assets/download.jpeg'
 function Card(){
     return (
         <div className="card">
-            <img className="card-image" src={profilePic} alt="profile picture"></img>
+            <img src={profilePic} alt="profile picture"></img>
             <h2 className="card-title">Bill</h2>
             <p className="card-text">Devops and coder</p>
         </div>
