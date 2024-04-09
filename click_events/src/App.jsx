1eb0e92dd9ada8f040362facd8ca5fe9afa9e6fa
William Crupi <crupib@gmail.com>
2024-04-09 10:49:39 -0400

    usestate - counter

--- click_events/src/App.jsx
+++ click_events/src/App.jsx
@@ -1,8 +1,9 @@
-import Button from './Button.jsx'
+//import Button from './Button.jsx'
+import ProfilePicture from './ProfilePicture.jsx'
 function App() {
 
   return (
-    <Button/>
+    <ProfilePicture/>
   )
 }
 
--- click_events/src/Button.jsx
+++ click_events/src/Button.jsx
@@ -1,8 +1,19 @@
 function Button() {
-   // const handleClick = () => console.log("OUCH!");
-    const handleClick2 = (name) => console.log(`${name} stop clicking me`);
+    // const handleClick2 = (name) => console.log(`${name} stop clicking me`);
+    //let count = 0;
+    /*const handleClick = (name) => {
+        if (count < 3) {
+            count++;
+            console.log(`${name} you clicked me ${count} times`);
+        }
+        else {
+            console.log(`${name} stop clicking me asswipe`)
+        }
+    }
+   */
+    const handleClick = (e) => e.target.textContent = "Fucking 😱";
     return( 
-        <button onClick={() => handleClick2("Asshole")}>Click Me 🤬</button>
-    )
+        <button onDoubleClick={(e) => handleClick(e)}>Click Me 🤬</button>
+    );
 }
 export default Button;
